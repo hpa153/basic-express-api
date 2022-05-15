e7f440cf72b484799c965828395235d571a69adc
Phuong Anh Hoang <71182218+hpa153@users.noreply.github.com>
2022-05-15 00:04:00 -0400

    added cache to all routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const apicache = require("apicache");
 
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
@@ -8,6 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+
+// Cache all routes
+const cache = apicache.middleware;
+app.use(cache("2 minutes"));
 
 // API uses the main route and router defined in the router layer
 app.use("/api/v1/workouts", v1WorkoutRouter);
--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,15 +1,13 @@
 const express = require("express");
-const apicache = require("apicache");
 
 // Controllers to handle HTTP related stuff
 const workoutController = require("../../controllers/workoutController");
 const recordController = require("../../controllers/recordController");
 
 const router = express.Router();
-const cache = apicache.middleware;
 
 // Endpoints to pass requests to controllers
-router.get("/", cache("2 minutes"), workoutController.getAllWorkouts);
+router.get("/", workoutController.getAllWorkouts);
 
 router.get("/:workoutId", workoutController.getOneWorkout);
 
